fbb9b3166529f5d351ca4ff0290c12dbfe1a132a
Florian Beck <info@florian-beck.de>
2023-07-12 14:48:08 +0200

    Made Uduino gameObject persistent between scenes

---
+++ Assets/script/UduinoPersistentInstance.cs
@@ -0,0 +1,37 @@
+using Uduino;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UduinoPersistentInstance : MonoBehaviour
+{
+    public static UduinoPersistentInstance instance;
+    // UduinoManager u;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    int counter;
+    void Update()
+    {
+        UduinoDevice myDevice = UduinoManager.Instance.GetBoard("Arduino");
+        UduinoManager.Instance.Read(myDevice, "readSensors"); // Read every frame the value of the "readSensors" function on our board.
+
+        if (SceneManager.GetActiveScene().name == "ReadSensor")
+        {
+            if (counter < 100)
+                counter++;
+            else
+                SceneManager.LoadScene("ReadSensor_Copy");
+        }
+    }
+}
