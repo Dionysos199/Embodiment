05dd51ee81dbb541bd1ab80d5ec9f833213fabef
Florian Beck <info@florian-beck.de>
2023-07-12 15:06:50 +0200

    Removed test code for scene change

--- Assets/script/UduinoPersistentInstance.cs
+++ Assets/script/UduinoPersistentInstance.cs
@@ -18,20 +18,4 @@ public class UduinoPersistentInstance : MonoBehaviour
         instance = this;
         DontDestroyOnLoad(gameObject);
     }
-
-    // Update is called once per frame
-    int counter;
-    void Update()
-    {
-        UduinoDevice myDevice = UduinoManager.Instance.GetBoard("Arduino");
-        UduinoManager.Instance.Read(myDevice, "readSensors"); // Read every frame the value of the "readSensors" function on our board.
-
-        if (SceneManager.GetActiveScene().name == "ReadSensor")
-        {
-            if (counter < 100)
-                counter++;
-            else
-                SceneManager.LoadScene("ReadSensor_Copy");
-        }
-    }
 }
